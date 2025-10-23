695b4024cdd7be5f0ce234c29cfc6557de2205ee
Rob Oliver <oliver.rw93@gmail.com>
2025-10-23 13:25:16 +0100

    day 9 improvements

--- src/day_09.rs
+++ src/day_09.rs
@@ -13,15 +13,15 @@ fn part_1(input: &str) -> u64 {
     let mut start = 0;
     let mut end = disk_contents.len() - 1;
     while start < end {
-        if disk_contents[end] == -1 {
-            end -= 1;
-        } else if disk_contents[start] != -1 {
-            start += 1;
-        } else {
-            disk_contents[start] = disk_contents[end];
-            disk_contents[end] = -1;
-            end -= 1;
-            start += 1;
+        match (disk_contents[start], disk_contents[end]) {
+            (_, None) => end -= 1,
+            (Some(_), _) => start += 1,
+            (None, Some(_)) => {
+                disk_contents[start] = disk_contents[end];
+                disk_contents[end] = None;
+                end -= 1;
+                start += 1;
+            }
         }
     }
     generate_checksum(&disk_contents)
@@ -31,16 +31,14 @@ fn part_2(input: &str) -> u64 {
     let mut disk_contents = parse_input(input);
     let mut start = 0;
     let mut end = disk_contents.len() - 1;
-    let mut file_id = -1;
+    let mut file_id = None;
     let mut file_size = 0;
     while start < end {
-        if file_id == -1 {
-            if disk_contents[end] == -1 {
-                end -= 1;
-            } else if disk_contents[start] != -1 {
-                start += 1;
-            } else {
-                file_id = disk_contents[end];
+        if let None = file_id {
+            match (disk_contents[start], disk_contents[end]) {
+                (_, None) => end -= 1,
+                (Some(_), _) => start += 1,
+                (None, Some(_)) => file_id = disk_contents[end],
             }
         } else {
             if file_id == disk_contents[end] {
@@ -51,10 +49,10 @@ fn part_2(input: &str) -> u64 {
                 if let Some(gap_start) = gap_start {
                     for i in 0..file_size {
                         disk_contents[gap_start + i] = disk_contents[end + 1 + i];
-                        disk_contents[end + 1 + i] = -1;
+                        disk_contents[end + 1 + i] = None;
                     }
                 }
-                file_id = -1;
+                file_id = None;
                 file_size = 0;
             }
         }
@@ -62,11 +60,11 @@ fn part_2(input: &str) -> u64 {
     generate_checksum(&disk_contents)
 }
 
-fn generate_checksum(disk_contents: &[i16]) -> u64 {
+fn generate_checksum(disk_contents: &[Option<u16>]) -> u64 {
     let mut checksum = 0;
     let mut count = 0;
     for &block in disk_contents {
-        if block != -1 {
+        if let Some(block) = block {
             checksum += count * u64::try_from(block).unwrap();
         }
         count += 1;
@@ -74,30 +72,35 @@ fn generate_checksum(disk_contents: &[i16]) -> u64 {
     checksum
 }
 
-fn find_gap(file_size: usize, mut start: usize, end: usize, disk_contents: &[i16]) -> Option<usize> {
+fn find_gap(
+    file_size: usize,
+    mut start: usize,
+    end: usize,
+    disk_contents: &[Option<u16>]
+) -> Option<usize> {
     let mut gap_size = 0;
     while start < end {
-        if disk_contents[start] == -1 {
+        if let Some(_) = disk_contents[start] {
+            gap_size = 0;
+        } else {
             gap_size += 1;
             if gap_size >= file_size {
                 return Some(start - (gap_size - 1));
             }
-        } else {
-            gap_size = 0;
         }
         start += 1;
     }
     None
 }
 
-fn parse_input(input: &str) -> Vec<i16> {
+fn parse_input(input: &str) -> Vec<Option<u16>> {
     let mut list = Vec::new();
     for (i, c) in input.chars().enumerate() {
         let size = c.to_digit(10).unwrap();
         let id = if i % 2 == 0 {
-            i16::try_from(i / 2).unwrap()
+            Some(u16::try_from(i / 2).unwrap())
         } else {
-            -1
+            None
         };
         for _ in 0..size {
             list.push(id);
